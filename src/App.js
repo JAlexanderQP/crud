7efcd1569536c75ffd3141d9358bb53ab88e7440
JAlexanderQP <alexanderqp@gmail.com>
2021-11-16 14:06:03 -0500

    Finalizando CRUD en memoria

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import shortid from "shortid";
 function App() {
   const [task, setTask] = useState(""); // Tarea Nueva
   const [tasks, setTasks] = useState([]); // Lista de tareas
+  const [editMode, setEditMode] = useState(false); //Para saber cuando se editar
+  const [id, setId] = useState(""); // Para identificar cual es el que se va a editar
+
   const addTask = (e) => {
     e.preventDefault();
     if (isEmpty(task)) {
-      console.log("Tarea vacia");
+      alert("El campo tarea esta vacio");
       return;
     }
 
@@ -24,9 +27,33 @@ function App() {
 
   const DeleteTask = (id) => {
     // alert("ID a eliminar: " + id);
-
     const filteredTask = tasks.filter((task) => task.id !== id);
     setTasks(filteredTask);
+  };
+
+  const editTask = (theTask) => {
+    // alert("ID a eliminar: " + id);
+    setTask(theTask.name);
+
+    setEditMode(true);
+    setId(theTask.id);
+  };
+
+  //Guarda la tarea a actaalizar
+  const saveTask = (e) => {
+    e.preventDefault();
+    if (isEmpty(task)) {
+      alert("El campo tarea esta vacio");
+      return;
+    }
+
+    const editedTasks = tasks.map((item) =>
+      item.id === id ? { id, name: task } : item
+    );
+    setTasks(editedTasks);
+    setEditMode(false);
+    setTask("");
+    setId("");
   };
 
   return (
@@ -35,8 +62,10 @@ function App() {
       <hr />
       <div className="row">
         <div className="col-12 col-md-4">
-          <h4 className="text-center">Formulario</h4>
-          <form onSubmit={addTask}>
+          <h4 className="text-center">
+            {editMode ? "Modificar tarea" : "Agregar tarea"}
+          </h4>
+          <form onSubmit={editMode ? saveTask : addTask} autocomplete="off">
             <input
               type="text"
               name="tarea"
@@ -46,8 +75,15 @@ function App() {
               onChange={(text) => setTask(text.target.value)}
               value={task}
             />
-            <button className="btn btn-dark btn-block" type="submit">
-              Agregar
+            <button
+              className={
+                editMode
+                  ? "btn btn-warning btn-block"
+                  : "btn btn-dark btn-block"
+              }
+              type="submit"
+            >
+              {editMode ? "Guardar" : "Agregar"}
             </button>
           </form>
           <br />
@@ -68,7 +104,10 @@ function App() {
                   >
                     Eliminar
                   </button>
-                  <button className="btn btn-warning btn-sm float-right">
+                  <button
+                    className="btn btn-warning btn-sm float-right"
+                    onClick={() => editTask(task)}
+                  >
                     Editar
                   </button>
                 </li>
