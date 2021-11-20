47a6804298b95e4b52ba7dcf1b56cc3fc6e66ce0
JAlexanderQP <alexanderqp@gmail.com>
2021-11-20 12:29:40 -0500

    CRUD finalizado con Dexie.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,110 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { isEmpty, size } from "lodash";
 import shortid from "shortid";
+import Dexie from "dexie";
+import Swal from "sweetalert2";
+import toast, { Toaster } from "react-hot-toast";
 
 function App() {
+  //Creo la Base de datos
+  //DEXIE
+  const db = new Dexie("bdTareas");
+  //Creo las collecciones (Tablas para guardar)
+  db.version(1).stores({
+    tareas: "id, task",
+  });
+  db.open().catch((err) => {
+    console.log(err.stack || err);
+  });
+
   const [task, setTask] = useState(""); // Tarea Nueva
   const [tasks, setTasks] = useState([]); // Lista de tareas
   const [editMode, setEditMode] = useState(false); //Para saber cuando se editar
   const [id, setId] = useState(""); // Para identificar cual es el que se va a editar
 
+  const fechaHoy = new Date();
+
+  useEffect(() => {
+    //Mostrar toas las tareas
+    //DEXIE MOSTRAR TODAS
+    const getTasks = async () => {
+      let allTask = await db.tareas.toArray();
+      setTasks(allTask);
+    };
+    getTasks();
+  }, []);
+
   const addTask = (e) => {
     e.preventDefault();
     if (isEmpty(task)) {
-      alert("El campo tarea esta vacio");
+      //alert("El campo tarea esta vacio");
+      toast.error("El campo tarea esta vacio");
       return;
     }
 
     const newTask = {
       id: shortid.generate(),
+      create: fechaHoy.toLocaleDateString(),
       name: task,
     };
 
     //Al arreglo de tareas agregame la nueva tarea
     setTasks([...tasks, newTask]);
+    //DEXIE GUARDAR
+    db.tareas.add(newTask).then(async () => {
+      let allTask = await db.tareas.toArray();
+      setTasks(allTask);
+    });
     setTask("");
   };
 
-  const DeleteTask = (id) => {
-    // alert("ID a eliminar: " + id);
-    const filteredTask = tasks.filter((task) => task.id !== id);
-    setTasks(filteredTask);
+  const DeleteTask = async (id) => {
+    Swal.fire({
+      title: "Atención",
+      text: "Estas seguro que quieres eliminar esta tarea",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      cancelButtonText: "Cancelar",
+      confirmButtonText: "Si, borrar tarea!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // alert("ID a eliminar: " + id);
+        //const filteredTask = tasks.filter((task) => task.id !== id);
+        //setTasks(filteredTask);
+
+        /*   db.tareas.delete(id);
+        let allTasks = db.tareas.toArray();
+        setTasks(allTasks); */
+        //DEXIE ELIMINAR
+        db.tareas.delete(id).then(async () => {
+          let allTask = await db.tareas.toArray();
+          setTasks(allTask);
+        });
+        setTask("");
+
+        Swal.fire(
+          "Eliminado!",
+          "La tarea fue eliminada correctamente.",
+          "success"
+        );
+      }
+    });
   };
 
-  const editTask = (theTask) => {
+  const editTask = async (theTask) => {
     // alert("ID a eliminar: " + id);
+
+    // await db.friends.put({ id: 4, name: "Foo", age: 33 });
     setTask(theTask.name);
+
+    //console.log(theTask.name);
+
+    /* db.tareas.update(theTask.id, { name: theTask.name }).then(async () => {
+      let allTask = await db.tareas.toArray();
+      setTasks(allTask);
+    }); */
 
     setEditMode(true);
     setId(theTask.id);
@@ -43,13 +114,19 @@ function App() {
   const saveTask = (e) => {
     e.preventDefault();
     if (isEmpty(task)) {
-      alert("El campo tarea esta vacio");
+      toast.error("El campo tarea esta vacio");
       return;
     }
 
     const editedTasks = tasks.map((item) =>
       item.id === id ? { id, name: task } : item
     );
+
+    //DEXIE ACTUALIZAR
+    db.tareas.update(id, { name: task }).then((updated) => {
+      if (updated) toast.success("Actualizado correctamente");
+      else toast.error("No se encontro el registro");
+    });
     setTasks(editedTasks);
     setEditMode(false);
     setTask("");
@@ -65,7 +142,7 @@ function App() {
           <h4 className="text-center">
             {editMode ? "Modificar tarea" : "Agregar tarea"}
           </h4>
-          <form onSubmit={editMode ? saveTask : addTask} autocomplete="off">
+          <form onSubmit={editMode ? saveTask : addTask} autoComplete="off">
             <input
               type="text"
               name="tarea"
@@ -116,6 +193,30 @@ function App() {
           )}
         </div>
       </div>
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+        gutter={8}
+        containerClassName=""
+        containerStyle={{}}
+        toastOptions={{
+          // Define default options
+          className: "",
+          duration: 5000,
+          style: {
+            background: "#363636",
+            color: "#fff",
+          },
+          // Default options for specific types
+          success: {
+            duration: 3000,
+            theme: {
+              primary: "green",
+              secondary: "black",
+            },
+          },
+        }}
+      />
     </div>
   );
 }
